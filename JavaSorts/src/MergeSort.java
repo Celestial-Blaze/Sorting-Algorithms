659207043ac160414e0d6243764c7aac3ca1719c
Celestial Blaze <152753428+Celestial-Blaze@users.noreply.github.com>
2025-05-13 14:10:55 -0400

    added Java merge sort (kinda)

---
+++ JavaSorts/src/MergeSort.java
@@ -0,0 +1,10 @@
+public class MergeSort {
+    public static void main(String[]args){
+        int[] myArray = {1,2,4,53,0,-22,42,-190,1000,29,37,3,12};
+
+    }
+    public int[] mergeSort(int[] alist){
+        int[] sorted = {1,2,3};
+     return sorted;
+    }
+}
